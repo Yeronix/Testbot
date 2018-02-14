496c1999792bff9a5fed300f0e063248699daa3a
Yeronix <20055216+Yeronix@users.noreply.github.com>
2018-02-14 13:26:48 -0600

    Add files via upload
    v1.0.3

--- bot.js
+++ bot.js
@@ -26,8 +26,6 @@ bot.on('ready', function (evt) {
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it needs to execute a command
-    // for this script it will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
@@ -37,10 +35,23 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch(cmd) {
             // !rage
             case 'rage':
-                bot.sendMessage({ to: channelID, message: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!' });
+                bot.sendMessage({ to: channelID, message: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' });
+            break;
+			// !gold
+			case 'gold':
+                bot.sendMessage({ to: channelID, message: 'I am the golden god!' });
+            break;
+			// !wow
+			case 'wow':
+                bot.sendMessage({ to: channelID, message: 'http://ksassets.timeincuk.net/wp/uploads/sites/55/2017/07/owen-wilson-wow-920x584.jpg' });
+            break;
+			// !crypto
+			case 'crypto':
+                bot.sendMessage({ to: channelID, message: 'https://thumbs.dreamstime.com/z/businessman-money-funny-displeased-count-holding-isolated-white-background-employee-young-man-small-profit-bankruptcy-56591282.jpg' });
             break;
             default:
                 bot.sendMessage({ to: channelID, message: 'Unknown command.' });
         }
     }
 })
+
